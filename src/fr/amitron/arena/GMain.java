6147041efe025a7a2bd03caaab6591dda173471c
Amitron-Dev <martingrellier79@gmail.com>
2024-07-18 16:11:37 +0200

    Préparation
    Préparation du projet

---
+++ src/fr/amitron/arena/GMain.java
@@ -0,0 +1,12 @@
+package fr.amitron.arena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GMain extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		System.out.println("§Arena Plugin");
+	}
+
+}
